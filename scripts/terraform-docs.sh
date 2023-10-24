0f83660efce286b461390775a8ea27a2c8a46a21
Russell Bunch <doomslayer@hpe.com>
2023-10-24 18:17:23 -0500

    Add terraform-docs update script

---
+++ scripts/terraform-docs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# MIT License
+#
+# (C) Copyright 2023 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+set -e
+
+if ! command -v terraform-docs >/dev/null 2>&1 ; then
+    echo 'Missing terraform-docs: https://github.com/terraform-docs/terraform-docs/#installation' >&2
+    exit 1
+fi
+
+if [ ! -d ./modules ]; then
+    echo 'Run this from the root of the repository.' >&2
+    exit 2
+fi
+
+for module in ./modules/*; do
+    terraform-docs markdown table "./$module"
+done
+
+echo 'Done.. do not forget to commit your changes :)'
